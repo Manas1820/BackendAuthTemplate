a666f353c6379664aede27e48351ad368e60b325
Manas1820 <manasgupta1820@gmail.com>
2021-04-02 04:08:16 +0530

    💨 added validation in auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const { registerValidation } = require('../validation');
+const { registerValidation, loginValidation } = require('../validation');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+// http://localhost:3000/api/user/register
 
 router.post('/register', async(req, res) => {
 
@@ -34,5 +36,30 @@ router.post('/register', async(req, res) => {
         res.status(400).json({ message: e });
     }
 });
+
+
+// http://localhost:3000/api/user/login
+
+router.post('/login', async(req, res) => {
+    // To check if the user entered details are valid or not
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // To check if the user is already in the database
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("email not found");
+
+
+    //To check if the password is correct
+    const check = await bcrypt.compare(req.body.password, user.password);
+    if (!check) return res.status(400).send("Incorrect Password");
+
+
+    // Create and assign a token
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+
+});
+
 
 module.exports = router;
